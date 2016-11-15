9c49f04f3350ceaaec3f2a69d4e96854f57289d6
Kenneth Adair <SOREASAN@GMAIL.COM>
2016-11-15 14:30:38 -0700

    Created Hello World

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+/*
+Kenneth Adair
+I discovered that on OSX to compile in the commandline
+a C++ file you type "c++ filename.cpp" and it will compile
+that way.  Using "gcc filename.cpp" gave me a gajillion errors
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+	return 0;
+}
