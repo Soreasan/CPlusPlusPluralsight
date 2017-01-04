1e8be1db56668a2303e2c999c4a7bcdb9412632e
Soreasan <soreasan@gmail.com>
2017-01-04 04:37:48 -0700

    Created a simple program that demonstrates casting variables to different types.

---
+++ Casting.cpp
@@ -0,0 +1,17 @@
+/*
+Kenneth Adair
+Sample code of some variable casts.  
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i1 = 1;
+    bool b1 = (bool) i1;
+    double d1 = 2.2;
+    int i2 = (int) d1;
+    int i3 = 1l;
+    return 0;
+}
