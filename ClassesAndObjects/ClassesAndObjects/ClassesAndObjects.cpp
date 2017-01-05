9fd324daae08da8418afed5b2fd91f148d2335dc
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-01-04 17:51:18 -0700

    Demoing Enumerations
    Following the C++ Fundamentals course, added some enumeration samples.

--- ClassesAndObjects/ClassesAndObjects/ClassesAndObjects.cpp
+++ ClassesAndObjects/ClassesAndObjects/ClassesAndObjects.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 //#include "Person.h"
 #include "Tweeter.h"
+#include "status.h"
 #include <iostream>
 
 using std::cout;
@@ -16,6 +17,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		Tweeter p2("Someone", "Else", 456, "@whoever");
 	}
 	cout<< "after innermost block" << endl;
+	Status s = Pending;
+	s = Approved;
 	return 0;
 }
 
---
+++ ClassesAndObjects/ClassesAndObjects/status.h
@@ -0,0 +1,6 @@
+enum Status
+{
+	Pending,
+	Approved,
+	Cancelled
+};
