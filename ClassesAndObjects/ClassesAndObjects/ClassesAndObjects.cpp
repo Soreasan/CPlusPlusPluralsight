1aa3b21202fe7cf6a748c79101b23e0cc40b8e63
Kenneth Adair <SOREASAN@GMAIL.COM>
2017-01-11 18:36:07 -0700

    Pointer demo
    Following a long with C++ Fundamentals on Pluralsight, followed a long
    with the pointer demo.

--- ClassesAndObjects/ClassesAndObjects/ClassesAndObjects.cpp
+++ ClassesAndObjects/ClassesAndObjects/ClassesAndObjects.cpp
@@ -79,6 +79,41 @@ int _tmain(int argc, _TCHAR* argv[])
 	cout << people.GetTotal() << endl;
 	/**************************************************************/
 
+	/************************ Pointers demo ***********************/
+	int a = 3;
+	cout << "a is " << a << endl;
+	int* pA = &a;
+	*pA = 4;
+	cout << "a is " << a << endl;
+	int b = 100;
+	pA = &b;
+	(*pA)++;
+	cout << "a " << a << ", *pA " << *pA << endl;
+
+	Person Kate("Kate", "Gregory", 234);
+	Person* pKate = &Kate;
+	(*pKate).setNumber(235);
+	cout << "Kate: " << Kate.GetName() << " " << Kate.GetNumber() << endl;
+	cout << "pKate: " << pKate->GetName() << " " << pKate->GetNumber() << endl;
+
+	int & rA = a;
+	rA = 5;
+	cout << "a is " << a << endl;
+
+	Person& rKate = Kate;
+	rKate.setNumber(345);
+	cout << "rKate: " << rKate.GetName() << " " << rKate.GetNumber() << endl;
+
+	//Bad pointer error handling
+	int* badPointer = nullptr;	//If we don't set the pointer to nullptr we can't do error handling
+	if (badPointer)
+	{
+		*badPointer = 3;
+		cout << *badPointer << endl;
+	}
+
+	/**************************************************************/
+
 	return 0;
 }
 
